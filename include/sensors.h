746f7f4d656639bb90d249a1468cf55fa383d1b9
jandrefrey <94923840+jandrefrey@users.noreply.github.com>
2024-08-07 23:33:13 +0200

    Added sensors measuring

--- include/sensors.h
+++ include/sensors.h
@@ -13,6 +13,25 @@
 
 /*** Includes ***/
 #include <Arduino.h>
+#include <Adafruit_LSM6DSO32.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
+#include <SparkFun_MMC5983MA_Arduino_Library.h>
+
+
+#define LSM_CS 10
+#define LSM_SCK 13
+#define LSM_MISO 12
+#define LSM_MOSI 11
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+#define MMC_CS 4
+//Other pins?
 
 /*** Class declaration ***/
 class Sensors
@@ -28,6 +47,13 @@ public:
 
 private:
 
+    int lsm32Init();
+    int bmp390Init();
+    int mmc598Init();
+
+    Adafruit_LSM6DSO32 dso32;
+    Adafruit_BMP3XX bmp;
+    SFE_MMC5983MA myMag;
 };
 
 extern Sensors m_sensors;
--- src/sensors.cpp
+++ src/sensors.cpp
@@ -24,6 +24,11 @@ Sensors::~Sensors()
 
 /*** Public Functions definitions ***/
 int Sensors::init() {
+    Serial.begin(9600);
+    lsm32Init();
+    bmp390Init();
+    mmc598Init();
+
     return 0;
 }
 
@@ -32,6 +37,37 @@ int Sensors::calib() {
 }
 
 int Sensors::measure() {
+    
+    //Accel and Gyro
+    sensors_event_t accel;
+    sensors_event_t gyro;
+    sensors_event_t temp;
+    dso32.getEvent(&accel, &gyro, &temp);
+    float accelx = accel.acceleration.x;        //acceleration is measured in m/s^2
+    float gyrox = gyro.gyro.x;                 //rotation is measured in rad/s
+    Serial.println("LSM32 Measured");
+
+    //Baro
+    if (bmp.performReading()) {
+        float pressure = bmp.pressure / 100.0;
+        float altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+    }
+
+    //Magnetometer
+    uint32_t currentX = 0;
+    uint32_t currentY = 0;
+    uint32_t currentZ = 0;
+    double scaledX = 0;
+    double scaledY = 0;
+    double scaledZ = 0;
+    myMag.getMeasurementXYZ(&currentX, &currentY, &currentZ);
+    scaledX = (double)currentX - 131072.0;
+    scaledX /= 131072.0;
+    scaledY = (double)currentY - 131072.0;
+    scaledY /= 131072.0;
+    scaledZ = (double)currentZ - 131072.0;
+    scaledZ /= 131072.0;
+
     return 0;
 }
 
@@ -41,6 +77,40 @@ int Sensors::filter() {
 
 
 /*** Private Functions definitions ***/
+int Sensors::lsm32Init() {
+    if (!dso32.begin_SPI(LSM_CS, LSM_SCK, LSM_MISO, LSM_MOSI)) {
+        Serial.println("LSM32 Error");
+    }
 
+    dso32.setAccelRange(LSM6DSO32_ACCEL_RANGE_32_G);
+    dso32.setGyroRange(LSM6DS_GYRO_RANGE_1000_DPS);
+
+    dso32.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
+    dso32.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
+
+    Serial.println("LSM6DSO32 init done");
+
+    return 0;
+}
+
+int Sensors::bmp390Init() {
+    if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+        Serial.println("BMP390 Error");
+    }
+
+    // Set up oversampling and filter initialization
+    bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+    bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+    bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+    bmp.setOutputDataRate(BMP3_ODR_200_HZ);
+}
+
+int Sensors::mmc598Init() {
+    if( !myMag.begin(MMC_CS)) {
+        Serial.println("MMC5983MA Error");
+    }
+
+    myMag.softReset();
+}
 
 Sensors m_sensors;
